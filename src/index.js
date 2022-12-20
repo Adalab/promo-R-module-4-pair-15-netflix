34f77af742e97773d68c705e1c49dae4f2f0ee27
Bruna <brunadasilvabz98@gmail.com>
2022-12-20 11:26:46 +0100

    filter gender

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+
+const movies = require("./data/movies.json");
 
 // create and config server
 const server = express();
@@ -11,4 +13,12 @@ const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
-console.log('holita');
+console.log("holita");
+
+server.get("/movies", (req, res) => {
+  console.log(req.query);
+  res.json({
+    success: "true",
+    movies: { movies },
+  });
+});
--- web/src/services/api-movies.js
+++ web/src/services/api-movies.js
@@ -1,36 +1,39 @@
 // login
 
-const getMoviesFromApi = () => {
-  console.log('Se están pidiendo las películas de la app');
+const getMoviesFromApi = (params) => {
+  console.log(params);
   // CAMBIA ESTE FETCH PARA QUE APUNTE A UN ENDPOINT DE TU SERVIDOR, PIENSA SI DEBE SER GET O POST, PIENSA QUÉ DATOS DEBES ENVIAR, ETC
-  return fetch('//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/empty.json')
-    .then(response => response.json())
-    .then(() => {
-      // CAMBIA EL CONTENIDO DE ESTE THEN PARA GESTIONAR LA RESPUESTA DEL SERVIDOR Y RETORNAR AL COMPONENTE APP LO QUE NECESITA
-      return {
-        success: true,
-        movies: [
-          {
-            id: '1',
-            title: 'Gambita de dama',
-            gender: 'Drama',
-            image:
-              '//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/images/gambito-de-dama.jpg'
-          },
-          {
-            id: '2',
-            title: 'Friends',
-            gender: 'Comedia',
-            image:
-              '//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/images/friends.jpg'
-          }
-        ]
-      };
+  return fetch(`http://localhost:4000/movies?gender=${params.gender}`, {
+    method: "GET",
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      return data;
     });
+  // CAMBIA EL CONTENIDO DE ESTE THEN PARA GESTIONAR LA RESPUESTA DEL SERVIDOR Y RETORNAR AL COMPONENTE APP LO QUE NECESITA
+  // return {
+  //   success: true,
+  //   movies: [
+  //     {
+  //       id: "1",
+  //       title: "Gambita de dama",
+  //       gender: "Drama",
+  //       image:
+  //         "//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/images/gambito-de-dama.jpg",
+  //     },
+  //     {
+  //       id: "2",
+  //       title: "Friends",
+  //       gender: "Comedia",
+  //       image:
+  //         "//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/images/friends.jpg",
+  //     },
+  //   ],
+  // };
 };
 
 const objToExport = {
-  getMoviesFromApi: getMoviesFromApi
+  getMoviesFromApi: getMoviesFromApi,
 };
 
 export default objToExport;
