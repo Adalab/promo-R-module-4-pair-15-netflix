ba19127a203fbfc68e9429b5008fc3024c250aab
Bella Irene <bellagarcia1985@gmail.com>
2022-12-23 11:22:50 +0100

    BD

--- README.md
+++ README.md
@@ -53,9 +53,9 @@ Por favor marca a continuación los ejericios que hayais terminado:
 
   4.5 Bases de datos I:
 
-- [ ] 1. Crear la base de datos
-- [ ] 2. Configura la base de datos en Node JS
-- [ ] 3. Haz un SELECT para obtener todas películas
+- [x] 1. Crear la base de datos
+- [x] 2. Configura la base de datos en Node JS
+- [x] 3. Haz un SELECT para obtener todas películas
 - [ ] 4. Mejora tu SELECT
 - [ ] 5. SELECT para el motor de plantillas
 - [ ] 6. Crear la tabla de usuarias
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 
-const movies = require("./data/movies.json");
+// const movies = require("./data/movies.json");
+const Database = require ('better-sqlite3');
+
+const db = new Database('./src/data/movies.db', {verbose:console.log});
+
+
 
 // create and config server
 const server = express();
@@ -15,21 +20,35 @@ server.listen(serverPort, () => {
 });
 console.log("holita");
 
+// server.get("/movies", (req, res) => {
+//   const response = {
+//     success: true,
+//     movies: movies,
+//   };
+//   res.json(response);
+// });
 server.get("/movies", (req, res) => {
+  const query = db.prepare('SELECT * FROM movies');
+  const list = query.all();
+  console.log(list);
   const response = {
-    success: true,
-    movies: movies,
-  };
-  res.json(response);
-});
+        success: true,
+        movies: list
+      };
+      res.json(response);
+    });
 
-server.get("/movie/:movieId", (req, res) => {
-  console.log(req.params);
-  console.log("holiiiis");
-  const findMovie = movies.find((movie) => movie.id === req.params.movieId);
-  console.log(findMovie);
-  res.json({}); //html
-});
+
+
+
+
+// server.get("/movie/:movieId", (req, res) => {
+//   console.log(req.params);
+//   console.log("holiiiis");
+//   const findMovie = movies.find((movie) => movie.id === req.params.movieId);
+//   console.log(findMovie);
+//   res.json({}); //html
+// });
 
 const staticServer = "./src/public-react";
 server.use(express.static(staticServer));
